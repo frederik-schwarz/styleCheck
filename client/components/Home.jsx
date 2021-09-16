04cf411e79d3699f25f054cb1ce38f6409b04080
Frederik Schwarz <frederik@jens-schwarz.com>
2021-09-16 15:41:29 +1200

    adjusted home screen for almost all screen sizes

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -10,7 +10,8 @@ function Home ({dispatch, history}) {
       <h1 className='titleHome'>Style Check</h1>
     </div>
     <div className='imgContainer'>
-      <img className='fashionImgHome' src='https://cdn.shopify.com/s/files/1/0022/5050/6349/files/Frame_2_1200x.png?v=1627911934' alt='fashion line up'>
+      <img className='fashionImgHome' src='https://cdn.shopify.com/s/files/1/0022/5050/6349/files/Frame_2_1200x.png?v=1627911934' alt='fashion line up'/>
+      <img className='fashionImgHome2' src='https://static01.nyt.com/images/2021/09/09/fashion/09AMERICAN-FASHION-pyer-moss/09AMERICAN-FASHION-pyer-moss-mobileMasterAt3x.jpg' alt='fashion line up'>
       </img>
       </div>
 
